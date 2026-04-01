136d5ede4612a8326215e9d63d6950786556b6d6
crisdux-cell <cristopherabrahamd@gmail.com>
2026-04-01 00:35:43 -0400

    Solucionando error de atributo agregar_producto

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import ProductoRopa, PedidoRopa # Cambia los nombres si los tuyos son diferentes
+
+admin.site.register(ProductoRopa)
+admin.site.register(PedidoRopa)
